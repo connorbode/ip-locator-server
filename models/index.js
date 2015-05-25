5ea15a5fc6aa95e76be8ff11c73b214a8bef0331
Connor Bode <connor.bode@gmail.com>
2015-05-25 19:19:25 -0400

    heroku-able db config

--- models/index.js
+++ models/index.js
@@ -6,7 +6,9 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, config)
+  : new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
 fs
